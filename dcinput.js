d85e66200d3e305fd13f777e7e1a75a1c9b182fa
DC <dcposch@dcpos.ch>
2015-03-12 23:14:37 -0400

    DCInput: cleaner code, comments, bugfixes. Support pointer lock

--- dcinput.js
+++ dcinput.js
@@ -1,56 +1,107 @@
 
+// Handles mouse and keyboard events. Tells you the current state
+// of the mouse and which keys are pressed, for easy of use in
+// game code (for example, in requestAnimationFrame())
+function DCInput(canvas) {
 
-var keyMap = {
-    38:'up', 40:'down', 37: 'right', 39:'left', //arrow keys
-    87:'up', 83:'down', 65: 'right', 68:'left', //wasd
-    81:'strafeleft', 69:'straferight' //qe
-};
-
-var keys = {}; //eg {'up':true} if up is currently pressed
-var mouse = {
-    'drag':false,
-    'last':{x:0,y:0},
-    'move':{x:0,y:0}
-};
-
-function onKeyUp(ev) {
-    var key = ev.keyCode;
-    if(keyMap[key]){
-        var mapped = keyMap[key];
-        keys[mapped] = false;
+    // Map key codes to clean event names
+    this.keyMap = {
+        38:'up', 40:'down', 37: 'right', 39:'left', //arrow keys
+        87:'up', 83:'down', 65: 'right', 68:'left', //wasd
+        81:'strafeleft', 69:'straferight' //qe
     }
-}
-function onKeyDown(ev){
-    var key = ev.keyCode;
-    if(keyMap[key]){
-        keys[keyMap[key]] = true;
-        console.log(keyMap[key]);
+
+    // Currently pressed keys, {'up':true} if up is currently pressed
+    this.keys = {} 
+
+    // Mouse status, last location, and how much it's moved since the last check
+    this.mouse = {
+        'pointerLock':false,
+        'drag':false,
+        'last':{x:0,y:0},
+        'move':{x:0,y:0}
     }
-}
 
-function onMouseUp(ev) {
-    mouse.drag = false;
-}
-function onMouseDown(ev) {
-    mouse.drag = mouse.last = {x:ev.screenX, y:ev.screenY};
-}
-function onMouseMove(ev) {
-    mouse.move = {x:ev.screenX-mouse.last.x, y:ev.screenY-mouse.last.y};
-    mouse.last = {x:ev.screenX, y:ev.screenY};
-}
-function onMouseEnter(ev) {
-}
-function onMouseLeave(ev) {
-    mouse.drag = false;
-}
+    this.getAndClearMouseMove = function() {
+        var ret = this.mouse.move
+        this.mouse.move = {x:0, y:0}
+        return ret
+    }
 
-$(function(){
-    $(window).keydown(onKeyDown);
-    $(window).keyup(onKeyUp);
-    $("canvas").mouseup(onMouseUp);
-    $("canvas").mousedown(onMouseDown);
-    $("canvas").mousemove(onMouseMove);
-    $("canvas").mouseenter(onMouseEnter);
-    $("canvas").mouseleave(onMouseLeave);
-});
+    this.requestPointerLock = function() {
+        var fn = canvas.requestPointerLock ||
+           canvas.mozRequestPointerLock ||
+           canvas.webkitRequestPointerLock
+        // Ask the browser to lock the pointer
+        fn.apply(canvas)
+    }
+
+
+    // Private event handlers
+
+    function onKeyUp(ev) {
+        var key = ev.keyCode
+        if(this.keyMap[key]){
+            var mapped = this.keyMap[key]
+            this.keys[mapped] = false
+        }
+    }
+    function onKeyDown(ev){
+        var key = ev.keyCode
+        if(this.keyMap[key]){
+            this.keys[this.keyMap[key]] = true
+            console.log(this.keyMap[key])
+        }
+    }
+
+    function onMouseUp(ev) {
+        this.mouse.drag = false
+    }
+    function onMouseDown(ev) {
+        this.mouse.drag = this.mouse.last = {x:ev.screenX, y:ev.screenY}
+    }
+    function onMouseMove(ev) {
+        var movementX, movementY;
+        if(this.mouse.pointerLock){
+            movementX = ev.movementX ||
+                ev.mozMovementX ||
+                ev.webkitMovementX ||
+                0
+            movementY = ev.movementY ||
+                ev.mozMovementY ||
+                ev.webkitMovementY ||
+                0
+        } else {
+          movementX = ev.screenX - this.mouse.last.x
+          movementY = ev.screenY - this.mouse.last.y
+        }
+        this.mouse.move.x += movementX
+        this.mouse.move.y += movementY
+        this.mouse.last.x = ev.screenX
+        this.mouse.last.y = ev.screenY
+    }
+    function onMouseEnter(ev) {
+    }
+    function onMouseLeave(ev) {
+        this.mouse.drag = false
+    }
+
+
+    function onPointerLock(ev) {
+        this.mouse.pointerLock = !!document.pointerLockElement
+    }
+
+
+    //Initialization
+    window.addEventListener('keydown', onKeyDown.bind(this), false)
+    window.addEventListener('keyup', onKeyUp.bind(this), false)
+    canvas.addEventListener('mousedown', onMouseDown.bind(this), false)
+    canvas.addEventListener('mouseup', onMouseUp.bind(this), false)
+    canvas.addEventListener('mousemove', onMouseMove.bind(this), false)
+    canvas.addEventListener('mouseenter', onMouseEnter.bind(this), false)
+    canvas.addEventListener('mouseleave', onMouseLeave.bind(this), false)
+    document.addEventListener('pointerlockchange', onPointerLock.bind(this), false)
+    document.addEventListener('mozpointerlockchange', onPointerLock.bind(this), false)
+    document.addEventListener('webkitpointerlockchange', onPointerLock.bind(this), false)
+}
 
--- voxmain.js
+++ voxmain.js
@@ -4,50 +4,62 @@
  * Trying out some voxel rendering
  */
 
-var CHUNK_WIDTH = 16;
-var CHUNK_HEIGHT = 64;
+// Constants
+// Each chunk is 16x16 voxels wide (X, Z) and 64 voxels high (Y)
+var CHUNK_WIDTH = 16
+var CHUNK_HEIGHT = 64
 
-var VOX_TYPE_AIR = 0;
-var VOX_TYPE_WATER = 1;
-var VOX_TYPE_GRASS = 2;
-var VOX_TYPE_STONE = 3;
+// There are different kinds of blocks
+var VOX_TYPE_AIR = 0
+var VOX_TYPE_WATER = 1
+var VOX_TYPE_GRASS = 2
+var VOX_TYPE_STONE = 3
 
-var chunks = [];
+// How fast you can move and look around
+var INPUT_SPEED = 25 // blocks per second
+var INPUT_SENSITIVITY = 0.01 // radians per pixel
+
+// Generate a demo voxel world
+var chunks = []
 for(var x = 0; x < 16*20; x+=CHUNK_WIDTH)
 for(var z = 0; z < 16*20; z+=CHUNK_WIDTH){
-    chunks.push(createChunk(x,z,0));
+    chunks.push(createChunk(x,z,0))
 }
 
-// http://dcpos.ch/canvas/dcgl/blocksets/simple.png
-var blockTexture;
-var blockTextureUVs = {};
-blockTextureUVs[VOX_TYPE_WATER] = {side:[13,12], top:[13,12], bottom:[13,12]};
-blockTextureUVs[VOX_TYPE_GRASS] = {side:[3,0], top:[0,0], bottom:[2,0]};
-blockTextureUVs[VOX_TYPE_STONE] = {side:[1,0], top:[1,0], bottom:[1,0]};
+// The DCInput object. See dcinput.js
+var input = null
+
+// Textures for every kind of block, all on one image.
+// See http://dcpos.ch/canvas/dcgl/blocksets/simple.png
+var blockTexture
+var blockTextureUVs = {}
+blockTextureUVs[VOX_TYPE_WATER] = {side:[13,12], top:[13,12], bottom:[13,12]}
+blockTextureUVs[VOX_TYPE_GRASS] = {side:[3,0], top:[0,0], bottom:[2,0]}
+blockTextureUVs[VOX_TYPE_STONE] = {side:[1,0], top:[1,0], bottom:[1,0]}
 
 
 // Generates a test chunk at the given coords and LOD
 function createChunk(x, z, lod) {
-    var data = new Uint8Array(CHUNK_WIDTH*CHUNK_WIDTH*CHUNK_HEIGHT);
+    var data = new Uint8Array(CHUNK_WIDTH*CHUNK_WIDTH*CHUNK_HEIGHT)
     for(var iy = 0; iy < CHUNK_HEIGHT; iy++)
     for(var ix = 0; ix < CHUNK_WIDTH; ix++)
     for(var iz = 0; iz < CHUNK_WIDTH; iz++) {
         var voxsize = 1<<lod;
-        var voxx = x + ix*voxsize;
-        var voxy = iy*voxsize;
-        var voxz = z + iz*voxsize;
+        var voxx = x + ix*voxsize
+        var voxy = iy*voxsize
+        var voxz = z + iz*voxsize
 
-        var voxtype;
+        var voxtype
         if(voxy < 10*((Math.sin(voxx/23) + Math.cos(voxz/19))+2)) {
         // if(voxy < 30 && voxy > 20 && ix > 1 && ix < 8 && iz > 1 && iz < 8) {
-            voxtype = VOX_TYPE_GRASS;
+            voxtype = VOX_TYPE_GRASS
         } else if (voxy < 15){
-            voxtype = VOX_TYPE_WATER;
+            voxtype = VOX_TYPE_WATER
         } else {
-            voxtype = VOX_TYPE_AIR;
+            voxtype = VOX_TYPE_AIR
         }
 
-        data[iy*CHUNK_WIDTH*CHUNK_WIDTH + ix*CHUNK_WIDTH + iz] = voxtype;
+        data[iy*CHUNK_WIDTH*CHUNK_WIDTH + ix*CHUNK_WIDTH + iz] = voxtype
     }
     return {
         x: x,
@@ -57,21 +69,25 @@ function createChunk(x, z, lod) {
     }
 }
 
+// Helper method for looking up a value from a packed voxel array (YXZ layout)
 function getVoxel(data, ix, iy, iz) {
     return data[iy*CHUNK_WIDTH*CHUNK_WIDTH + ix*CHUNK_WIDTH + iz]
 }
 
+// Helper method for writing up a value from a packed voxel array (YXZ layout)
 function setVoxel(data, ix, iy, iz, val) {
     data[iy*CHUNK_WIDTH*CHUNK_WIDTH + ix*CHUNK_WIDTH + iz] = val
 }
 
+// Meshes a chunk, and sends the mesh (as a vertex buffer + UV buffer)
+// to the GPU
 function loadChunkToGPU(chunk) {
-    if(chunk.gl) return;
+    if(chunk.gl) return
 
     // greedy meshing algo
     // http://0fps.net/2012/06/30/meshing-in-a-minecraft-game/
-    var verts = [], uvs = [];
-    var meshed = new Uint8Array(chunk.data.length);
+    var verts = [], uvs = []
+    var meshed = new Uint8Array(chunk.data.length)
     for(var iy = 0; iy < CHUNK_HEIGHT; iy++)
     for(var ix = 0; ix < CHUNK_WIDTH; ix++)
     for(var iz = 0; iz < CHUNK_WIDTH; iz++) {
@@ -81,9 +97,9 @@ function loadChunkToGPU(chunk) {
         if (isMeshed > 0) continue
 
         // expand to largest possible quad
-        var jy = iy;
-        var jx = ix;
-        var jz = iz;
+        var jy = iy
+        var jx = ix
+        var jz = iz
         for(; jy < CHUNK_HEIGHT; jy++) {
             var jvoxtype = getVoxel(chunk.data, jx, jy, jz)
             if (jvoxtype !== voxtype) break
@@ -114,10 +130,10 @@ function loadChunkToGPU(chunk) {
         // add the six faces (12 tris total) for the quad
         var voxsize = 1 << chunk.lod
         var voxx = chunk.x + ix*voxsize
-        var voxy = iy*voxsize;
+        var voxy = iy*voxsize
         var voxz = chunk.z + iz*voxsize
         var voxx2 = chunk.x + jx*voxsize
-        var voxy2 = jy*voxsize;
+        var voxy2 = jy*voxsize
         var voxz2 = chunk.z + jz*voxsize
         for(var fside = 0; fside < 2; fside++) {
             var xface = fside === 1 ? voxx2 : voxx
@@ -183,13 +199,28 @@ function loadChunkToGPU(chunk) {
     var uvBuffer = gl.createBuffer()
     gl.bindBuffer(gl.ARRAY_BUFFER, uvBuffer)
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(uvs), gl.STATIC_DRAW)
-    var vertexCount = verts.length / 3;
-    console.log("Loaded chunk to GPU. "+vertexCount+" verts");
+    var vertexCount = verts.length / 3
+    console.log("Loaded chunk to GPU. "+vertexCount+" verts")
     chunk.gl = {
         vertexBuffer: vertexBuffer,
         vertexCount: vertexCount,
         uvBuffer: uvBuffer
-    };
+    }
+}
+
+// Handles all keyboard and mouse input
+// Lets you move and look around
+function handleInput() {
+    if(input.keys.up) moveXZ(dt*INPUT_SPEED, dir + Math.PI)
+    if(input.keys.down) moveXZ(dt*INPUT_SPEED, dir)
+    if(input.keys.left) moveXZ(dt*INPUT_SPEED, dir + Math.PI*0.5)
+    if(input.keys.right) moveXZ(dt*INPUT_SPEED, dir + Math.PI*1.5)
+
+    var move = input.getAndClearMouseMove()
+    if(!input.mouse.drag && !input.mouse.pointerLock) return
+    azith -= move.y*INPUT_SENSITIVITY
+    azith = Math.min(0.4*Math.PI, Math.max(-0.4*Math.PI, azith))
+    dir -= move.x*INPUT_SENSITIVITY
 }
 
 function moveXZ(r, theta) {
@@ -197,40 +228,29 @@ function moveXZ(r, theta) {
     loc[2] += Math.cos(theta) * r
 }
 
-function handleInput() {
-    var speed = 15
-    if(keys.up) moveXZ(dt*speed, dir + Math.PI)
-    if(keys.down) moveXZ(dt*speed, dir)
-    if(keys.left) moveXZ(dt*speed, dir + Math.PI*0.5)
-    if(keys.right) moveXZ(dt*speed, dir + Math.PI*1.5)
-
-    if(!mouse.drag) return
-    var sensitivity = 0.01
-    azith -= mouse.move.y*sensitivity
-    azith = Math.min(0.4*Math.PI, Math.max(-0.4*Math.PI, azith))
-    dir -= mouse.move.x*sensitivity
-}
-
+// Renders one frame
+// Expects initGL() to already have been called, 
+// and voxel chunks to already be loaded to the GPU
 function renderFrame(canvas){
     // scale, clear window
-    var width = canvas.clientWidth;
-    var height = canvas.clientHeight;
-    canvas.width = width;
-    canvas.height = height;
-    gl.viewport(0, 0, width, height); 
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    var width = canvas.clientWidth
+    var height = canvas.clientHeight
+    canvas.width = width
+    canvas.height = height
+    gl.viewport(0, 0, width, height) 
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 
     // setup camera
-    mat4.perspective(50, width / height, 1.0, 1000.0, pmat);
+    mat4.perspective(50, width / height, 1.0, 1000.0, pmat)
 
     // setup matrixes
-    mat4.identity(mvmat);
-    mat4.rotate(mvmat, -azith, [1,0,0]);
-    mat4.rotate(mvmat, -dir, [0,1,0]);
-    mat4.translate(mvmat, [-loc[0], -loc[1], -loc[2]]);
+    mat4.identity(mvmat)
+    mat4.rotate(mvmat, -azith, [1,0,0])
+    mat4.rotate(mvmat, -dir, [0,1,0])
+    mat4.translate(mvmat, [-loc[0], -loc[1], -loc[2]])
 
     // draw some voxels
-    setShaders("vert_texture", "frag_voxel");
+    setShaders("vert_texture", "frag_voxel")
     setUniforms()
     var posVertexPosition = getAttribute("aVertexPosition")
     var posVertexUV = getAttribute("aVertexUV")
@@ -241,9 +261,9 @@ function renderFrame(canvas){
         if (!chunk.gl) return
 
         // bind textures (already copied to GPU)
-        gl.activeTexture(gl.TEXTURE0);
-        gl.bindTexture(gl.TEXTURE_2D, blockTexture);
-        gl.uniform1i(posSampler, 0);
+        gl.activeTexture(gl.TEXTURE0)
+        gl.bindTexture(gl.TEXTURE_2D, blockTexture)
+        gl.uniform1i(posSampler, 0)
 
         // bind verts and uvs (already copied to GPU)
         gl.bindBuffer(gl.ARRAY_BUFFER, chunk.gl.vertexBuffer)
@@ -252,19 +272,23 @@ function renderFrame(canvas){
         gl.vertexAttribPointer(posVertexUV, 2, gl.FLOAT, false, 0, 0)
 
         gl.drawArrays(gl.TRIANGLES, 0, chunk.gl.vertexCount)
-    });
+    })
 }
 
 function main() {
     var canvas = document.getElementById("gl")
+    input = new DCInput(canvas)
+    canvas.addEventListener('click', input.requestPointerLock.bind(input))
+
     initGL(canvas)
     chunks.forEach(function(chunk){
-        loadChunkToGPU(chunk);
+        loadChunkToGPU(chunk)
     })
     blockTexture = loadTexture("blocksets/simple.png", function(){
         animate(function(){
-            handleInput();
-            renderFrame(canvas);
-        }, canvas);
+            handleInput()
+            renderFrame(canvas)
+        }, canvas)
     })
 }
+
