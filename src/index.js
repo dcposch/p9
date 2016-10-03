e1b8119f79fbfef42dddc0a9aa9bc63ff5edfdc4
DC <dcposch@dcpos.ch>
2016-10-02 17:51:21 -0700

    sound wip

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 var regl = require('./regl') // Create a regl context from the <canvas> element
 var drawTriangle = require('./draw-triangle') // Precompile regl commands
+var sound = require('./sound')
 
 console.log('WELCOME ~ VOXEL WORLD')
+
+// Click to enter full screen
+document.querySelector('#gl').addEventListener('click', start)
+function start () {
+  sound.play('win95.mp3')
+}
 
 // Start the requestAnimationFrame render loop
 regl.frame(function (context) {
---
+++ src/sound.js
@@ -0,0 +1,23 @@
+module.exports = {
+  play
+}
+
+var fs = require('fs')
+
+// Cache <audio> elements for instant playback
+var cache = {}
+
+// Preload the cache
+var names = fs.readdirSync('static/sounds')
+names.forEach(function (name) {
+  var audio = new window.Audio()
+  audio.src = 'sounds/' + name
+  cache[name] = audio
+})
+
+function play (name) {
+  let audio = cache[name]
+  if (!audio) throw new Error('Missing sound: ' + name)
+  audio.currentTime = 0
+  audio.play()
+}
